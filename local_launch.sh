6bb7c6c654583d672b45878af3670ce73d0d38f3
Jon Zimbel <jzimbel@gmail.com>
2017-03-26 19:03:11 -0400

    #security

--- local_launch.sh
+++ local_launch.sh
@@ -2,6 +2,7 @@
 
 export SPOTIPY_REDIRECT_URI='https://www.google.com'
 export SPOTIPY_CLIENT_ID='76159224cd0e4725826934d253118290'
+# pls no steal
 export SPOTIPY_CLIENT_SECRET='fd3f120077a3491898bb444c5a23beef'
 
 python app.py
